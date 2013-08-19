a8ae3f6d68e2d407d84150f6a182e8b20f6ca646
ilya_scherbinin <m0n9oose@gmail.com>
2013-08-19 09:58:47 +0400

    change observer after_create override to alias_chain

--- lib/loader_issue_observer.rb
+++ lib/loader_issue_observer.rb
@@ -1,15 +1,19 @@
 module LoaderIssueObserver
   def self.included(base)
     base.class_eval do
-      def after_create(issue)
-        unless issue.subject =~ /_imported/
-          Mailer.issue_add(issue).deliver if Setting.notified_events.include?('issue_added')
-        end
-      end
+      base.send(:include, InstanceMethods)
+
+      alias_method_chain :after_create, :notify_about_import
 
       def after_save(issue)
         issue.update_column(:subject, issue.subject.gsub('_imported', '')) if issue.subject =~ /_imported/
       end
+    end
+  end
+
+  module InstanceMethods
+    def after_create_with_notify_about_import(issue)
+      after_create_without_notify_about_import(issue) unless issue.subject =~ /_imported/
     end
   end
 end
